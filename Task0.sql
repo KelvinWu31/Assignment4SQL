927e36043ea067a6e7f6105c3b93d3e7441f6b2f
Kelvin Wu <kelvinwu31@gmail.com>
2020-08-11 10:08:14 -0400

    Create Task0.sql
    This file contains the sql query to each question.

---
+++ Task0.sql
@@ -0,0 +1,15 @@
+Task 0
+
+1. SELECT population FROM world
+  WHERE name = 'Germany'
+/* change name to germany to see population */
+
+2.SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+/* changed name to Sweden, Norway and Denmark to see population */
+
+3.SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+
+/* changed the population size to 200000 AND 250000 to find country that fit that population size */
